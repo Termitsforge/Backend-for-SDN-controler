a755272e757a9b08139c8fc09ac61b1b5d206efe
orehov <orehov@zelax.ru>
2022-11-24 14:33:32 +0300

    send json frontend

--- app.js
+++ app.js
@@ -3,20 +3,6 @@ const httpListener = require("./httpListener");
 const host = "localhost";
 const port = 8000;
 const listener = new httpListener();
-
-
-// let a = function() {
-//   var childProcess = require("child_process");
-//   var oldSpawn = childProcess.spawn;
-//   function mySpawn() {
-//       console.log('spawn called');
-//       console.log(arguments);
-//       var result = oldSpawn.apply(this, arguments);
-//       return result;
-//   }
-//   childProcess.spawn = mySpawn;
-// };
-// a();
 /**
  * Переменная сервера
  * https://nodejs.org/api/http.html#httpcreateserveroptions-requestlistener
--- httpListener.js
+++ httpListener.js
@@ -3,21 +3,22 @@
  * Главная задача определить входящий запрос и перенаправить на нужный обработчик.
  */
 const runCommand = require("./runCommand");
- module.exports = class httpListener {
-    constructor(){
-
+module.exports = class httpListener {
+    constructor() {
+        this.returnObject;
     }
     /**
      * Главная функция обработки запросов.
      */
     listen(req, res) {
-        if(req.url === "/testURI") {
-            let returnObject = runCommand("dir", ["/B"]);
-            returnObject.data = returnObject.data.split("\r\n").filter(Boolean);
-            returnObject.stderr = returnObject.stderr.split("\r\n").filter(Boolean).join('');
-            console.log(returnObject);
+        if (req.url === "/testURI") {
+            this.returnObject = runCommand("dir", ["/B"]);
+            this.returnObject.data = this.returnObject.data.split("\r\n").filter(Boolean);
+            this.returnObject.stderr = this.returnObject.stderr.split("\r\n").filter(Boolean).join('');
+
+            res.setHeader("200", "Content-Type", 'application/json')
+            res.end(JSON.stringify(this.returnObject));
         };
-        res.end("server is worked!");
     }
 
 }
