c8666e292e10e4f6a67256cf0fd3001389eb7890
orehov <orehov@zelax.ru>
2022-11-24 13:50:02 +0300

    add async func

--- runCommand.js
+++ runCommand.js
@@ -1,25 +1,53 @@
 /**
  * Модуль выполнения команд в терминале
  * Модуль реализует один публичный метод аргументом которого является строка команды.
- * https://nodejs.org/api/child_process.html#child_processspawnsynccommand-args-options
  */
 
 const childProcess = require("child_process");
 
 let returnObject = {
+    error: "",
     stderr: "",
     data: [],
     finalCode: 0,
 };
 const defaults = {
-    shell: true, 
+    shell: true,
   };
 /**
- * 
+ * Функция выполнения команды в теримнале
+ * Выполнение функции будет асинхронное.
  * @param {*} command  - выполняемая команда
- * @param {*} arg  - аргументы команды. Аргументы должны быть перечислены в массиве.
+ * @param {*} arg  - аргументы команды. Аргументы должны быть перечислены в массиве
  */
-module.exports = function runCommand(command, arg = []) {
+module.exports = function runCommandAsync(command, arg = []) {
+    const childProcessRunCommand = childProcess.spawn(command, arg, defaults);
+
+    childProcessRunCommand.stdout.on("data", data => {
+        returnObject.data.push(data.toString());
+    });
+
+    childProcessRunCommand.stderr.on("data", data => {
+        returnObject.stderr = data;
+    });
+
+    childProcessRunCommand.on("error", error => {
+        returnObject.error = error;
+    });
+
+    childProcessRunCommand.on("close", code => {
+        returnObject.finalCode = code;
+    });
+
+    return returnObject;
+};
+/**
+ * Функция выполнения команды в теримнале
+ * Выполнение функции будет синхронное.
+ * @param {*} command  - выполняемая команда
+ * @param {*} arg  - аргументы команды. Аргументы должны быть перечислены в массиве
+ */
+module.exports = function runCommandSync(command, arg = []) {
     const childProcessRunCommand = childProcess.spawnSync(command, arg, defaults);
     returnObject.data = childProcessRunCommand.stdout.toString();
     returnObject.stderr = childProcessRunCommand.stderr.toString();
